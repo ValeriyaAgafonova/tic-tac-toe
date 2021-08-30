649d2b92558064742652256e5752c737b0aaebe6
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2021-08-30 16:37:05 +0300

    fix: solution  Please enter the commit message for your changes. Lines starting

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,24 @@
 class TicTacToe {
+currentPlayer = 'x';
+gameField = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
     constructor() {
 
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer
     }
-
+    
     nextTurn(rowIndex, columnIndex) {
-
+        //if (this.gameField[rowIndex][columnIndex]){
+        //    return;
+        //}
+this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
     }
 
     isFinished() {
@@ -20,15 +30,15 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+return this.gameField.every(row => row.every(col => col))
     }
 
     isDraw() {
-
+return this.noMoreTurns() && !this.getWinner();
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+return this.gameField[rowIndex][colIndex];
     }
 }
 
